4763324642a76f4fde95c36d075b0506d6f2b53b
Pierre Lasante <pierre.lasante@videotron.ca>
2013-10-08 18:22:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bargain::Application.config.secret_key_base = '01ecc6f8b5e93697be4a13525a6fbff90e8d7b10432453120ebd079cd0f3ae11b43ce17cc8942cde11301f9d42e64a48c994e32744476e91cf738c9962242982'
