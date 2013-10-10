bd35d05b559462c79280e8d304f5df32d84a3547
Pierre Lasante <pierre.lasante@videotron.ca>
2013-10-10 18:36:32 -0400

    Reached Test Fixtures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+  # To check that those fields are not null or empty ""
+  validates :title, :description, :image_url, presence: true
+  
+  # To check that price is at least 1 cent
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  
+  validates :title, uniqueness: true
+  
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
